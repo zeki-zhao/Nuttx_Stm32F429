3e1e75dd1ebef08c640ae91999ef5dc1987ccde1
zeki <715716752@qq.com>
2025-01-04 18:17:27 +0800

    1、添加lcd触控驱动 2、添加Snake游戏

---
+++ Nuttx_Stm32F429/apps/builtin/builtin_proto.h
@@ -0,0 +1,13 @@
+int fboverlay_main(int argc, char *argv[]);
+int nx_main(int argc, char *argv[]);
+int nsh_main(int argc, char *argv[]);
+int sh_main(int argc, char *argv[]);
+int hidkbd_main(int argc, char *argv[]);
+int Test_main(int argc, char *argv[]);
+int mount_main(int argc, char *argv[]);
+int Snake_main(int argc, char *argv[]);
+int myi2c_main(int argc, char *argv[]);
+int fb_main(int argc, char *argv[]);
+int i2c_main(int argc, char *argv[]);
+int mytouch_main(int argc, char *argv[]);
+int nxterm_main(int argc, char *argv[]);
